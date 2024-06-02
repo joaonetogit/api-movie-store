6c119ffb085ddef4b54dd89950fed522ddc6dff2
João Domingos Pinheiro Neto <56659658+joaonetogit@users.noreply.github.com>
2024-06-02 13:10:44 -0300

    feat: host images on amazon s3

--- api/products.js
+++ api/products.js
@@ -1,45 +1,44 @@
 const express = require("express");
 const app = express();
-const path = require("path");
 
-app.use("/public", express.static(path.join(__dirname, "../public")));
+const URL = "https://images-movie-store.s3.sa-east-1.amazonaws.com";
 
 const products = [
   {
     id: 1,
     title: "Viúva Negra",
-    price: 9.99,
-    image: "/public/viuva-negra.png",
+    price: 39.99,
+    image: `${URL}/viuva-negra.png`,
   },
   {
     id: 2,
     title: "Shang-Chi",
-    price: 30.99,
-    image: "/public/shang-chi.png",
+    price: 31.99,
+    image: `${URL}/shang-chi.png`,
   },
   {
     id: 3,
     title: "Homem Aranha",
-    price: 29.99,
-    image: "/public/spider-man.png",
+    price: 19.99,
+    image: `${URL}/spider-man.png`,
   },
   {
     id: 5,
     title: "Morbius",
-    price: 9.99,
-    image: "/public/morbius.png",
+    price: 5.99,
+    image: `${URL}/morbius.png`,
   },
   {
     id: 6,
     title: "Batman",
-    price: 30.99,
-    image: "/public/batman.png",
+    price: 60.99,
+    image: `${URL}/batman.png`,
   },
   {
     id: 4,
     title: "Eternos",
-    price: 29.99,
-    image: "/public/eternals.png",
+    price: 22.99,
+    image: `${URL}/eternals.png`,
   },
 ];
 
