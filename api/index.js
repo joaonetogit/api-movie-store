2041f2bd08843a5eac4ee47a0ea6ec608beca8f6
João Domingos Pinheiro Neto <56659658+joaonetogit@users.noreply.github.com>
2024-07-01 17:01:28 -0300

    fix: adjust cors

--- api/index.js
+++ api/index.js
@@ -8,21 +8,32 @@ dotenv.config();
 
 const port = process.env.PORT || 3001;
 
-app.use(
-  cors({
-    origin: ["http://localhost:5173", "https://movie-store-xi.vercel.app/"],
-    methods: ["GET", "PUT", "POST", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  }),
-);
+app.use(cors({
+  origin: ["http://localhost:5173", "https://movie-store-xi.vercel.app"],
+  methods: ["GET", "PUT", "POST", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+
+app.options("*", cors());
 
 app.use(router);
 
 mongoose
   .connect(
-    "mongodb+srv://joaoneto:bMxoHjO1HrdvQ6Lv@cluster0.blbxvay.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    process.env.MONGODB_URI,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
   )
   .then(() => {
     console.log("Successfully connected to MongoDB Atlas!");
