e06cdb9b9d8006c70e92982522b2021aa7acbf22
João Domingos Pinheiro Neto <56659658+joaonetogit@users.noreply.github.com>
2024-07-05 12:34:18 -0300

    fix: adjust cors

--- api/index.js
+++ api/index.js
@@ -8,25 +8,9 @@ dotenv.config();
 
 const port = process.env.PORT || 3001;
 
+app.use(cors());
+
 app.use(express.json());
-
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  }),
-);
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS",
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
 
 app.use("/api", router);
 
